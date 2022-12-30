9a61455220cfc49888d47eb43b36cb735ed970a6
Valerii Komar <inrarad@gmail.com>
2022-12-30 18:43:16 +0000

    Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog raggy = new Dog();
+        Dog baggy = new Dog();
+        Dog daggy = new Dog();
+    }
+}
