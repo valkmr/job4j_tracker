21d7aa13c6b660facddf7dd4784f2db7af7a1f1e
Valerii Komar <inrarad@gmail.com>
2022-12-30 19:57:06 +0000

    Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveName(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("name - " + this.name + "\nfood - " + this.food);
+    }
+
+    public static void main(String[] args) {
+        Cat one = new Cat();
+        one.giveName("black");
+        one.eat("milk");
+        one.show();
+    }
+}
