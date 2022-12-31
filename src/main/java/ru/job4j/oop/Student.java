1ccc49f75a815661a7966ca69649d168f104cd62
Valerii Komar <inrarad@gmail.com>
2022-12-31 16:51:02 +0000

    Вызов метода объекта [#166085 #376331]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -5,8 +5,15 @@ public class Student {
         System.out.println("I believe I can fly");
     }
 
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
     public static void main(String[] args) {
         Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
         petya.song();
         petya.song();
         petya.song();
