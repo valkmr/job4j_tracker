afddfc1bc3fcc436184703ed7fd0fe471d6de1ec
Valerii Komar <inrarad@gmail.com>
2023-01-05 13:28:45 +0000

    Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -11,6 +14,10 @@ public class Item {
     }
 
     public Item(int id, String name) {
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return created;
     }
 
     public int getId() {
