26e0c0bd41835c02a68dbe8bf1fa5e0d393a179c
Valerii Komar <inrarad@gmail.com>
2023-01-05 10:48:17 +0000

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,15 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+    }
+
+    public Item(int id, String name) {
+    }
+
     public int getId() {
         return id;
     }
