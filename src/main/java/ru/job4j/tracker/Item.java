0f303ac804b057de532e712ec20d4f91c9ec41cc
Valerii Komar <inrarad@gmail.com>
2023-01-06 18:47:10 +0000

    Перегрузка конструктора [#173890 #377733]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -11,9 +11,12 @@ public class Item {
     }
 
     public Item(String name) {
+        this.name = name;
     }
 
     public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public LocalDateTime getLocalDateTime() {
