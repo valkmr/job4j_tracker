73816156abf8cccdffd618eb42e6757584aaaf49
Valerii Komar <inrarad@gmail.com>
2023-01-06 21:05:40 +0000

    Модель данных. [#174683]

---
+++ src/main/java/pojo/College.java
@@ -0,0 +1,15 @@
+package pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Valerii Komar");
+        student.setGroup("Intern");
+        student.setDate(new Date(1671044160000L));
+        System.out.println(student.getFullName());
+        System.out.println(student.getGroup());
+        System.out.println(student.getDate());
+    }
+}
---
+++ src/main/java/pojo/Student.java
@@ -0,0 +1,33 @@
+package pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fullName;
+    private String group;
+    private Date date;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
