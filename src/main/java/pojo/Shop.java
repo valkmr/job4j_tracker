b4bf040ff8e0c222411f8c6f3b7ab6c3a473a030
Valerii Komar <inrarad@gmail.com>
2023-01-07 17:24:54 +0000

    Массив с пустыми ячейками [#176470]

---
+++ src/main/java/pojo/Shop.java
@@ -0,0 +1,14 @@
+package pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        int rsl = -1;
+        for (int index = 0; index < products.length; index++) {
+            if (products[index] == null) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
