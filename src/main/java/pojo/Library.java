180a109d37ceb53987e9ba91c63e2bb8d0013fdf
Valerii Komar <inrarad@gmail.com>
2023-01-07 17:12:17 +0000

    Массивы и модели. [#174892]

---
+++ src/main/java/pojo/Library.java
@@ -0,0 +1,30 @@
+package pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book programingBook = new Book("Java 9.0 to 17.0", 618);
+        Book javaEducation = new Book("Mastering Java", 657);
+        Book javaClean = new Book("Clean code", 327);
+        Book environment = new Book("Beginning Intellij IDEA", 283);
+        Book webDevWithJava = new Book("Java for Web Development", 2353);
+        Book softTest = new Book("Effective Software Testing", 329);
+        Book[] books = {programingBook, javaEducation, javaClean, environment, webDevWithJava, softTest};
+        for (int index = 0; index < books.length; index++) {
+            System.out.printf("name: %-27s pages: %-10s %n", books[index].getName(), books[index].getPages());
+        }
+        System.out.println();
+        Book pr;
+        pr = books[0];
+        books[0] = books[3];
+        books[3] = pr;
+        for (int index = 0; index < books.length; index++) {
+            System.out.printf("name: %-27s pages: %-10s %n", books[index].getName(), books[index].getPages());
+        }
+        System.out.println();
+        for (int index = 0; index < books.length; index++) {
+            if (books[index].getName().equals("Clean code")) {
+                System.out.printf("name: %-27s pages: %-10s %n", books[index].getName(), books[index].getPages());
+            }
+        }
+    }
+}
