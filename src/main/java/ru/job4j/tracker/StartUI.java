1114fb078be55da898a5b51b6a514e9efe5ec4ba
Valerii Komar <inrarad@gmail.com>
2023-01-08 19:58:10 +0000

    toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,5 +6,6 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         System.out.println(item.getLocalDateTime().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        System.out.println(item);
     }
 }
