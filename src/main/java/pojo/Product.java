26b57ff4df81cf0df68439537becf96629e6d20b
Valerii Komar <inrarad@gmail.com>
2023-01-11 10:42:47 +0000

    Массивы и модели. [#174892 #378608]

--- src/main/java/pojo/Product.java
+++ src/main/java/pojo/Product.java
@@ -6,23 +6,6 @@ public class Product {
     private String name;
     private int count;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Product product = (Product) o;
-        return count == product.count && Objects.equals(name, product.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, count);
-    }
-
     public Product(String name, int count) {
         this.name = name;
         this.count = count;
@@ -42,5 +25,22 @@ public class Product {
 
     public void setCount(int count) {
         this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
     }
 }
