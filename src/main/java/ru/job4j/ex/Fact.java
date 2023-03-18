1a7736f71d811f9fa7be6e7f1dc7d89f419d7c08
Valerii Komar <inrarad@gmail.com>
2023-03-18 15:18:55 +0000

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -6,7 +6,7 @@ public class Fact {
             throw new IllegalArgumentException("N could not bee less then 0");
         }
         int rsl = 1;
-        for (int index = 0; index <= n; index++) {
+        for (int index = 2; index <= n; index++) {
             rsl *= index;
         }
         return rsl;
